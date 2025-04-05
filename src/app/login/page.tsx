596975c1bcf41c1f92c2d5d92ce5cf3798b7fba5
hemaxpatel <hemaxpatel3124@gmail.com>
2025-04-05 19:02:56 +0530

    feat: enhance login and signup pages with form fields and navigation links

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,47 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+// import {axios} from "axios";
+
 export default function LoginPage() {
-    return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    )
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login Page</h1>
+      <hr />
+      <label htmlFor="email">Email</label>
+      <input
+        className="border-2 rounded-md p-2 mb-4"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="border-2 rounded-md p-2 mb-4"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        className="bg-blue-500 text-white rounded-md p-2 mb-4"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+      <Link href="/signup" className="text-white">Sign Up Here</Link>
+
+    </div>
+  );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,57 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+// import {axios} from "axios";
+
 export default function SignupPage() {
-    return (
-        <div>
-            <h1>Signup Page</h1>
-        </div>
-    )
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup Page</h1>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input
+        className="border-2 rounded-md p-2 mb-4"
+        type="text"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="border-2 rounded-md p-2 mb-4"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="border-2 rounded-md p-2 mb-4"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        className="bg-blue-500 text-white rounded-md p-2 mb-4"
+        onClick={onSignup}
+      >
+        Sign Up
+      </button>
+      <Link href="/login" className="text-white">Login Here</Link>
+
+    </div>
+  );
 }
