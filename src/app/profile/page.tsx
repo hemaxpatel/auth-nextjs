38f8a6dc3c9a5d6fd997d822a2525781d4f209f8
hemaxpatel <hemaxpatel3124@gmail.com>
2025-04-07 10:27:05 +0530

    feat: implement user data retrieval and display on profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 import { toast } from "react-hot-toast";
@@ -6,6 +7,8 @@ import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter();
+
+  const [data, setData] = useState("nothing");
 
   const logout = async () => {
     try {
@@ -18,10 +21,31 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserData = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile Page</h1>
       <hr />
+      <h2>
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>ID:{data}</Link>
+        )}
+      </h2>
+
+      <button
+        onClick={getUserData}
+        className="bg-green-600 text-white rounded-md p-2 mb-4"
+      >
+        User Details
+      </button>
+
       <button
         onClick={logout}
         className="bg-blue-500 text-white rounded-md p-2 mb-4"
