70419def48273565220ec66540aeae7a3344607b
hemaxpatel <hemaxpatel3124@gmail.com>
2025-04-07 09:17:56 +0530

    feat: add logout functionality with API endpoint and update profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logged out successfully");
+      router.push("/login");
+    } catch (error: any) {
+      console.error("Logout failed", error);
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile Page</h1>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 text-white rounded-md p-2 mb-4"
+      >
+        Log Out
+      </button>
     </div>
   );
 }
